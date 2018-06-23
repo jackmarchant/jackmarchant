43d3979ce945e71a5e7dd98a7a9201e0f27519c6
Jack Marchant <jack@expert360.com>
2018-06-23 21:14:41 +1000

    update versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule JackMarchant.Mixfile do
     [
       app: :jack_marchant,
       version: "1.0.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
