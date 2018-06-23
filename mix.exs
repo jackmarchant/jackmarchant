20aed39799e5904223902d96b9f4a43d7961e7cc
Jack Marchant <jack@expert360.com>
2018-06-23 21:13:16 +1000

    update image static path, elixir/erlang versions

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule JackMarchant.Mixfile do
   def project do
     [
       app: :jack_marchant,
-      version: "0.0.1",
-      elixir: "~> 1.5",
+      version: "1.0.0",
+      elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
