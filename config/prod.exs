8b460138660a1025c07539418a66efac4dfc1787
Jack Marchant <jack@expert360.com>
2018-09-08 11:41:54 +1000

    ssl host

--- config/prod.exs
+++ config/prod.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :jack_marchant, JackMarchantWeb.Endpoint,
   load_from_system_env: true,
   http: [port: System.get_env("PORT")],
-  url: [host: "jackmarchant.com", port: System.get_env("PORT")],
+  url: [host: "www.jackmarchant.com", port: System.get_env("PORT")],
   cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
